00a83d463aeae6675032924d9ccea51732214ef2
James Sweetland <56346421+JamesSweetland@users.noreply.github.com>
2020-01-24 18:40:53 +0000

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,16 @@
-// Code goes here
+angular.module('routerTestApp', ['ui.router']);
 
-    // load with
-    // http://local9.experiments.lifeafterflex.com/com/dotComIt/samples/uiRouter/01Index_TwoViews.html#/state1
-
-    angular.module('routerTestApp', ['ui.router']);
-
-    angular.module('routerTestApp').config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider){
-        // set default state
-        $urlRouterProvider.otherwise("/state1");
-        // set up all states
-        $stateProvider
-                .state('state1', {
-                    url: '/state1',
-                    templateUrl : 'scripts/states/state1.html'
-                })
-                .state('state2', {
-                    url: '/state2',
-                    templateUrl : 'scripts/states/state2.html'
-                })
-    }]);
+angular.module('routerTestApp').config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider){
+    // set default state
+    $urlRouterProvider.otherwise("/state1");
+    // set up states
+    $stateProvider
+        .state('state1', {
+            url: '/state1',
+            templateUrl : 'scripts/states/state1.html'
+    })
+        .state('state2', {
+            url: '/state2',
+            templateUrl : 'scripts/states/state2.html'
+    })
+}]);
