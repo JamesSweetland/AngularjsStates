bd91b46826b775f7f0b3dddd658d33e5867431d7
James Sweetland <56346421+JamesSweetland@users.noreply.github.com>
2020-01-24 18:12:52 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Code goes here
+
+    // load with
+    // http://local9.experiments.lifeafterflex.com/com/dotComIt/samples/uiRouter/01Index_TwoViews.html#/state1
+
+    angular.module('routerTestApp', ['ui.router']);
+
+    angular.module('routerTestApp').config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider){
+        // set default state
+        $urlRouterProvider.otherwise("/state1");
+        // set up all states
+        $stateProvider
+                .state('state1', {
+                    url: '/state1',
+                    templateUrl : 'state1.html'
+                })
+                .state('state2', {
+                    url: '/state2',
+                    templateUrl : 'state2.html'
+                })
+    }]);
